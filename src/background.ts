e97e6b13cfc5a07b04955ea1e566684eb1d9823e
Gannochenko <gannochenko.sv@gmail.com>
2022-10-18 00:41:41 +0200

    wip

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,7 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
         console.log(
             `Storage! key "${key}" in namespace "${namespace}" changed.`,
-            `Old value was "${oldValue}", new value is "${newValue}".`
+            `Old value was "${oldValue}", new value is "${newValue}".`,
         );
     }
 });
--- src/components/Application/hooks/useApplication.ts
+++ src/components/Application/hooks/useApplication.ts
@@ -4,18 +4,28 @@ export const useApplication = () => {
     const [sent, setSent] = useState(false);
 
     const onCopyTokenButtonClick = useCallback(() => {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            if (tabs[0].id) {
-                chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-                    console.log(response.farewell);
-                });
+        chrome.tabs.query(
+            { active: true, currentWindow: true },
+            function (tabs) {
                 setSent(true);
-            }
-        });
+                console.log('!');
+                if (tabs[0].id) {
+                    chrome.tabs.sendMessage(
+                        tabs[0].id,
+                        { greeting: 'hello' },
+                        function (response) {
+                            console.log(response.farewell);
+                        },
+                    );
+                }
+            },
+        );
     }, []);
 
     return {
-        copyTokenProps: onCopyTokenButtonClick,
+        copyTokenProps: {
+            onClick: onCopyTokenButtonClick,
+        },
         showSent: sent,
     };
 };
